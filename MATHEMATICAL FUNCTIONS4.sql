5652bac8dbba9eac4035eac23bde9913564697ee
Afisha Azme <azmeshaik@gmail.com>
2026-03-07 01:15:30 +0530

    Add files via upload

---
+++ MATHEMATICAL FUNCTIONS4.sql
@@ -0,0 +1,40 @@
+--Find square of a number.1225
+select square(75)
+select square (10.5)
+--Find cube of a number
+select 6*6*6 as cube
+select power(6,3)
+--Calculate factorial of a number(skip)
+--Find greatest value among three numbers
+select greatest(5,4,3)
+--Find logarithm (base 10) of a number.
+--Find sine value of an angle
+select sin(30)
+--find cosine value of an angle
+select cos(45)
+--find tangent value
+select tan(30)
+--Convert degrees to radians
+select radians(60)
+--convert radians to degree
+select DEGREES(60)
+--Find logarithm (base 10) of a number.
+select log10(20)
+--Find natural log of a number.
+select log(10)
+--Find least value among three numbers.
+select least(50,27,21)
+--Calculate variance of sales.(--skip)
+select var(sales) from ...
+--Calculate standard deviation of sales
+select stdev(sales) from ......
+--Find average deviation.
+select avg().....(--skip)
+--Calculate geometric mean.
+select EXP(AVG(LOG(Sales))) AS Geometric_Mean from......
+--Calculate harmonic mean.
+--select (skip) 
+--Find sum of squares.
+select sum(square(24))
+--Calculate correlation between two columns.
+--Calculate regression slope
